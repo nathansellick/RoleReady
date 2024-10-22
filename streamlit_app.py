26de98a2397b487de8b1175d657e4167bb0cfd8b
nathansellick <nsellick3@gmail.com>
2024-10-22 15:25:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
+# CSS for dark blue background
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #00008B;  /* Dark Blue color */
+        color: white;  /* Change text color to white for better contrast */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("Role Ready")
 st.write(
     "Match. Tailor. Succeed."
 )
+
+
