e110e55057d678d8accfd1ec54e97f550b03af14
Nathan Sellick <nathansellick@rockborne.com>
2024-10-23 09:51:31 +0100

    Home page with logo and multiple tabs created

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 # CSS for dark blue background
 st.markdown(
@@ -13,9 +14,24 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.title("Role Ready")
-st.write(
-    "Match. Tailor."
-)
+# Create multiple tabs
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home","Resume", "Job Search", "Saved", "Help"])
+
+with tab1: 
+
+    # Custom CSS to change the title font color to white without affecting the background
+    st.markdown("""
+    <style>
+    h1 {
+        color: white;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    # Open and display an image from your local directory
+    image = Image.open('role_ready_logo.png') 
+    st.image(image, caption='This is a local image.', use_column_width=True)
+
+
 
 
